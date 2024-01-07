a928f023258caa0f4603bd3513feac26a59d2fd5
feng_5201314 <1037395462@qq.com>
2024-01-07 22:47:28 +0800

    fix: 导出表格增加换行样式

--- src/hooks/useExcel.ts
+++ src/hooks/useExcel.ts
@@ -6,23 +6,19 @@ const useExcel = () => {
   const exportExcel = useCallback((data: any[], fileName = '工作日历') => {
     const worksheet = utils.json_to_sheet(data);
     const workbook = utils.book_new();
-    console.log('worksheet', worksheet, worksheet['!ref']!.split(':')[0].charCodeAt(0));
-    // 假设 ws 是你的工作表
 
-    // for(let )
-    for (let R = worksheet['!ref']!.split(':')[0].charCodeAt(0); R <= worksheet['!ref']!.split(':')[1].charCodeAt(0); ++R) {
+    // 导出表格增加换行样式
+    for (let R = worksheet['!ref']!.split(':')[0].charCodeAt(1); R <= worksheet['!ref']!.split(':')[1].charCodeAt(1); ++R) {
       const cell_address = { c: 2, r: R - 49 }; // 假设 c 列是第3列（即字母C，所以数字索引为2）
       const cell_ref = utils.encode_cell(cell_address);
 
       // 检查单元格是否存在且包含换行符
-      if (worksheet[cell_ref] && typeof worksheet[cell_ref].w === 'string' && worksheet[cell_ref].w.includes('\n')) {
+      if (worksheet[cell_ref] && typeof worksheet[cell_ref].v === 'string' && worksheet[cell_ref].v.includes('\n')) {
         // 设置自动换行样式
         if (!worksheet[cell_ref].s) worksheet[cell_ref].s = {};
         worksheet[cell_ref].s.wrapText = true;
       }
     }
-
-    console.log('worksheet2', worksheet);
 
     utils.book_append_sheet(workbook, worksheet, fileName);
 
